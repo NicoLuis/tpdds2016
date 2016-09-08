2a1d660a258d6b5f42a282ea77caf551153a6be5
miltonbrandes <miltonbrandess@hotmail.com>
2016-09-08 17:38:56 -0300

    Update CreacionUsuario.sql

--- CreacionUsuario.sql
+++ CreacionUsuario.sql
@@ -1,2 +1,11 @@
+use dds2016
 INSERT INTO [dbo].[usuario]
-VALUES('jBenitez1', 'gJlj/JeZnA3NwuQKcArdVg====', 'julieta', 'benitez', 'False')
+VALUES('admin', '4F8Q91ePWMvdNEbFmKiQqA==', 'admin', 'admin', 'True')
+INSERT INTO [dbo].[usuario]
+VALUES('jBenitez', 'gJlj/JeZnA3NwuQKcArdVg====', 'julieta', 'benitez', 'False')
+INSERT INTO [dbo].[usuario]
+VALUES('mbrandes', '1OPhWTD/4aug+pbX3XjYLA====', 'milton', 'brandes', 'True')
+INSERT INTO [dbo].[usuario]
+VALUES('nLuis', 'vYjr/vXuVXjIZUkN8egLDA====', 'nicolas', 'luis', 'false')
+INSERT INTO [dbo].[usuario]
+VALUES('user', '+nChvldIy7NubRsewRbHUA====', 'user', 'user', 'false')

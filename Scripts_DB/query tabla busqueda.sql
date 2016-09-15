f5b9d88fecf47ebebf837925bce8cd98d9f2ba3f
miltonbrandes <miltonbrandess@hotmail.com>
2016-09-15 16:37:37 -0300

    tabla busqueda

---
+++ Scripts_DB/query tabla busqueda.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO dbo.busquedas VALUES ('admin',	'nombre: Honda', 4, 2015-12-03,'Honda Pilar, Concesionario Honda, Honda Autos, Buena Honda')
+INSERT INTO dbo.busquedas VALUES ('admin',	'nombre: Yamaha',	2,	2015-12-03,	'Concesionario Yamaha, Yamaha Motors')
+INSERT INTO dbo.busquedas VALUES ('admin',	'nombre: pepe4',	0,	2015-12-25, '' 	)
